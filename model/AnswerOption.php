a274be71d030a9e5d3caaf73bae190ae790e0369
Donato <donatopirozzi@gmail.com>
2016-03-14 16:17:52 +0100

    Provided the type range within the Question-wc

--- model/AnswerOption.php
+++ model/AnswerOption.php
@@ -4,6 +4,7 @@ class AnswerOption {
 
     var $label;
     var $type = null;
+    var $checked = false;
 }
 
 ?>
--- model/XmlParser.php
+++ model/XmlParser.php
@@ -29,13 +29,15 @@ function findAndParseTemplate($xml, $execution, $templateId) {
                 $task = new Task();
                 $task->imageUrl = (string) $execAttrs["image"];
                 $task->question = (string) $tempTask->question;
+
                 $task->answerType = (string) $tempTask->answer->attributes()["type"];
-                $task->answerOptions = parseAnswerOptions($tempTask);
 
                 $task->valueFrom = (string) $tempTask->answer->attributes()["valueFrom"];
                 $task->valueTo = (string) $tempTask->answer->attributes()["valueTo"];
                 $task->labelFrom = (string) $tempTask->answer->attributes()["labelFrom"];
                 $task->labelTo = (string) $tempTask->answer->attributes()["labelTo"];
+
+                $task->answerOptions = parseAnswerOptions($task, $tempTask);
 
                 array_push($tasks, $task);
             }
@@ -46,8 +48,19 @@ function findAndParseTemplate($xml, $execution, $templateId) {
     return null;
 }//EndFunction.
 
-function parseAnswerOptions($tempTask) {
+function parseAnswerOptions($taskInstance, $tempTask) {
     $options = [];
+
+    if (strcmp($taskInstance->answerType, 'range') == 0) {
+        for ($i=$taskInstance->valueFrom; $i <=$taskInstance->valueTo; $i++) {
+            $ansOpt = new AnswerOption();
+            $ansOpt->label = $i . "";
+            array_push($options, $ansOpt);
+        }
+
+        return $options;
+    }
+
     foreach ($tempTask->answer->option as $j => $tempTaskOption) {
         $option = new AnswerOption();
         $option->label = (string) $tempTaskOption->attributes()["label"];
