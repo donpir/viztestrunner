827294a6046dfaee992489a49b9fb3bd03ce45ff
Donato <donatopirozzi@gmail.com>
2016-03-14 10:15:49 +0100

    Added the question image

--- html/Question.php
+++ html/Question.php
@@ -6,13 +6,29 @@
     <link rel="import" href="../bower_components/polymer/polymer.html">
     <script type="text/javascript" src="../bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
 
+    <link rel="import" href="../bower_components/iron-flex-layout/iron-flex-layout.html">
+
     <link rel="import" href="question-wc.html">
 
 </head>
 <body>
 
-    <question-wc id="questionwc"></question-wc><br/>
-    <input type="button" value="Confirm" id="btnConfirm" onclick="btnConfirmClick()" />
+    <style is="custom-style">
+        .flex-horizontal {
+        @apply(--layout-horizontal);
+        }
+        .flexchild {
+        @apply(--layout-flex);
+        }
+    </style>
+
+    <div class="container flex-horizontal">
+        <div style="border: 1px solid;"><image id="imgVisualization" src=""></image></div>
+        <div style="margin-left: 6px;" class="flexchild">
+            <question-wc id="questionwc"></question-wc><br/>
+            <input type="button" value="Confirm" id="btnConfirm" onclick="btnConfirmClick()" />
+        </div>
+    </div>
 
     <script type="text/javascript">
         var qidx = 0;
@@ -51,6 +67,9 @@
 
             var questionwc = document.getElementById("questionwc");
             questionwc.question = jsonResponse.data;
+
+            var imagewc = document.getElementById("imgVisualization");
+            imagewc.setAttribute("src", "../experiment/" + jsonResponse.data.imageUrl);
         };
     </script>
 
